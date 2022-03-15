1c7688f2a38b72ce3352e723a944e1d101047e51
AleksiSalminen <aleksisalminen1234@gmail.com>
2022-03-15 23:02:09 +0200

    Removed player distance calc function

--- src/frontend/js/main/camera.js
+++ src/frontend/js/main/camera.js
@@ -47,7 +47,6 @@ function Camera(canvas, resolution, focalLength) {
 /** The main rendering method */
 
 Camera.prototype.render = function (player, players, level) {
-  this.updatePlayerDistances(player, players);
   this.drawSky(player.pos.rotation, level.light);
   this.drawColumns(player, players, level);
   this.drawWeapon();
@@ -257,71 +256,6 @@ Camera.prototype.objectProject = function (height, angle, distance) {
     height: wallHeight
   };
 };
-
-/** Player distance calculations */
-
-Camera.prototype.updatePlayerDistances = function (player, players) {
-  const plPos = player.pos;
-  let pl2Pos = { x: 0, y: 0 };
-  const angle = plPos.rotation;
-
-  // Geometric variables
-  let lineF = {};
-  let distanceFromPlayer = 0;
-  let distanceFromLine = 0;
-  let lineLength = 0;
-  let lineToP2Angle = 0;
-
-  /** Calculate the line function */
-
-  // Create an example position
-  const linePos = {
-    x: Math.cos(angle) * 1,
-    y: Math.sin(angle) * 1
-  };
-
-  // Define the line function
-  lineF.slope = (plPos.y - linePos.y) / (plPos.x - linePos.x);
-  lineF.stdTerm = plPos.y - lineF.slope * plPos.x;
-
-  /** Calculate and update (other) players' distance info */
-  for (let plI = 0; plI < players.length; plI++) {
-    if (players[plI].number !== player.number) {
-      pl2Pos = players[plI].pos;
-
-      // Distance from the player
-      distanceFromPlayer = Math.sqrt(
-        (Math.pow(pl2Pos.x - plPos.x, 2)) +
-        (Math.pow(pl2Pos.y - plPos.y, 2))
-      );
-
-      // Distance from the player line
-      distanceFromLine =
-        Math.abs(lineF.slope * pl2Pos.x + pl2Pos.y + lineF.stdTerm)
-        /
-        Math.sqrt(Math.pow(lineF.slope, 2) + 1)
-        ;
-
-      // Line length
-      lineLength = Math.sqrt(Math.abs(
-        Math.pow(distanceFromPlayer, 2) -
-        Math.pow(distanceFromLine, 2)
-      ));
-
-      // The angle between the player line and player 2
-      lineToP2Angle = Math.atan(distanceFromLine / lineLength);
-
-      // Update distances information
-      pl2Pos.distances = {
-        fromPlayer: distanceFromPlayer,
-        fromLine: distanceFromLine,
-        lineLength: lineLength,
-        lineToP2Angle: lineToP2Angle
-      };
-
-    }
-  }
-}
 
 
 /** Exports */
