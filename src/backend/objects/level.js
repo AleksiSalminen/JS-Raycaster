331421168eccefe2f1b851a4d3ce73fdbc0ca9ec
AleksiSalminen <aleksisalminen1234@gmail.com>
2022-03-16 16:19:47 +0200

    Level object creation working

--- src/backend/objects/level.js
+++ src/backend/objects/level.js
@@ -27,8 +27,8 @@ function parseLevelInfo (data) {
             while (data[i+1] !== "WallGrid:") {
                 i++;
                 wallTextures.push({
-                    id: data.slice(0, 2),
-                    texture: data.slice(4, data.length)
+                    id: data[i].slice(0, 2),
+                    texture: data[i].slice(4, data[i].length)
                 });
             }
             info.wallTextures = wallTextures;
@@ -43,15 +43,35 @@ function parseLevelInfo (data) {
         }
     }
 
+    let formattedWalls = [];
+    for (let k = 0;k < info.walls.length;k++) {
+        for (let h = 0;h < info.walls[k].length;h+=2) {
+            let line = info.walls[k];
+            let value;
+            if (line[h] === "0") {
+                value = parseInt(line[h+1]);
+            }
+            else {
+                value = parseInt(line[h] + line[h+1]);
+            }
+            formattedWalls.push(value);
+        }
+    }
+    info.walls = formattedWalls;
+
     return info;
 }
 
 class Level {
     constructor (fileName, objects, enemies) {
-        this.fileName = fileName;
         const data = helpers.getDataFromFile(LEVEL_FILES_LOC + fileName);
         const levelInfo = parseLevelInfo(data);
-        console.log(levelInfo);
+        
+        this.fileName = fileName;
+        this.name = levelInfo.name;
+        this.skybox = levelInfo.skybox;
+        this.wallTextures = levelInfo.wallTextures;
+        this.walls = levelInfo.walls;
     }
 }
 
