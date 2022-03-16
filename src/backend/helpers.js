5569acba2aef8d21f8f9172ac674cbd5d0f7a4e7
AleksiSalminen <aleksisalminen1234@gmail.com>
2022-03-16 16:29:00 +0200

    Getters and setters for Level class, moved one function to helpers

--- src/backend/helpers.js
+++ src/backend/helpers.js
@@ -3,19 +3,77 @@ const fs = require("fs");
 
 module.exports = {
 
-    makeid (length) {
-        let result = "";
-        let characters =
-          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    },
+  makeid(length) {
+    let result = "";
+    let characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  },
 
-    getDataFromFile (fileName) {
-      return fs.readFileSync(fileName, "utf8");
+  getDataFromFile(fileName) {
+    return fs.readFileSync(fileName, "utf8");
+  },
+
+  parseLevelInfo(data) {
+    let info = {};
+    // Split the lines to an array
+    data = data.split("\n");
+    for (let j = 0; j < data.length; j++) {
+      data[j] = data[j].replace("\r", "");
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      let line = data[i];
+      if (line == "Name:") {
+        i++;
+        info.name = data[i]
+      }
+      else if (line == "Skybox:") {
+        i++;
+        info.skybox = data[i]
+      }
+      else if (line == "WallTextures:") {
+        let wallTextures = [];
+        while (data[i + 1] !== "WallGrid:") {
+          i++;
+          wallTextures.push({
+            id: data[i].slice(0, 2),
+            texture: data[i].slice(4, data[i].length)
+          });
+        }
+        info.wallTextures = wallTextures;
+      }
+      else if (line == "WallGrid:") {
+        let walls = [];
+        while (i < data.length - 1) {
+          i++;
+          walls.push(data[i]);
+        }
+        info.walls = walls;
+      }
+    }
+
+    let formattedWalls = [];
+    for (let k = 0; k < info.walls.length; k++) {
+      for (let h = 0; h < info.walls[k].length; h += 2) {
+        let line = info.walls[k];
+        let value;
+        if (line[h] === "0") {
+          value = parseInt(line[h + 1]);
+        }
+        else {
+          value = parseInt(line[h] + line[h + 1]);
+        }
+        formattedWalls.push(value);
+      }
+    }
+    info.walls = formattedWalls;
+
+    return info;
   },
 
 };
--- src/backend/objects/level.js
+++ src/backend/objects/level.js
@@ -4,68 +4,10 @@ const helpers = require("../helpers");
 const LEVEL_FILES_LOC = "../def/levels/"
 
 
-function parseLevelInfo (data) {
-    let info = {};
-    // Split the lines to an array
-    data = data.split("\n");
-    for (let j = 0;j < data.length;j++) {
-        data[j] = data[j].replace("\r", "");
-    }
-    
-    for (let i = 0;i < data.length;i++) {
-        let line = data[i];
-        if (line == "Name:") {
-            i++;
-            info.name = data[i]
-        }
-        else if (line == "Skybox:") {
-            i++;
-            info.skybox = data[i]
-        }
-        else if (line == "WallTextures:") {
-            let wallTextures = [];
-            while (data[i+1] !== "WallGrid:") {
-                i++;
-                wallTextures.push({
-                    id: data[i].slice(0, 2),
-                    texture: data[i].slice(4, data[i].length)
-                });
-            }
-            info.wallTextures = wallTextures;
-        }
-        else if (line == "WallGrid:") {
-            let walls = [];
-            while (i < data.length-1) {
-                i++;
-                walls.push(data[i]);
-            }
-            info.walls = walls;
-        }
-    }
-
-    let formattedWalls = [];
-    for (let k = 0;k < info.walls.length;k++) {
-        for (let h = 0;h < info.walls[k].length;h+=2) {
-            let line = info.walls[k];
-            let value;
-            if (line[h] === "0") {
-                value = parseInt(line[h+1]);
-            }
-            else {
-                value = parseInt(line[h] + line[h+1]);
-            }
-            formattedWalls.push(value);
-        }
-    }
-    info.walls = formattedWalls;
-
-    return info;
-}
-
 class Level {
     constructor (fileName, objects, enemies) {
         const data = helpers.getDataFromFile(LEVEL_FILES_LOC + fileName);
-        const levelInfo = parseLevelInfo(data);
+        const levelInfo = helpers.parseLevelInfo(data);
         
         this.fileName = fileName;
         this.name = levelInfo.name;
@@ -73,6 +15,32 @@ class Level {
         this.wallTextures = levelInfo.wallTextures;
         this.walls = levelInfo.walls;
     }
+    
+
+    /** Getters */
+    getFileName () { return this.fileName; }
+    getName () { return this.name; }
+    getSkyBox () { return this.skybox; }
+    getWallTextures () { return this.wallTextures; }
+    getWalls () { return this.walls; }
+
+    /** Setters */ 
+    setFileName (newFileName) {
+        if (newFileName && newFileName.length > 0) { this.fileName = newFileName; }
+    }
+    setName (newName) {
+        if (newName && newName.length > 0) { this.name = newName; }
+    }
+    setSkyBox (newSkyBox) {
+        if (newSkyBox && newSkyBox.length > 0) { this.skybox = newSkyBox; }
+    }
+    setWallTextures (newWallTextures) {
+        if (newWallTextures && newWallTextures.length > 0) { this.wallTextures = newWallTextures; }
+    }
+    setWalls (newWalls) {
+        if (newWalls && newWalls.length > 0) { this.walls = newWalls; }
+    }
+
 }
 
 module.exports = Level;
